4d48f3d7951256e4b234e11077ec227c64e48b43
Ilia Znaamenskii <ilia.znamenskii@sentinels.ai>
2023-08-01 13:55:31 +0200

    update

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,13 +1,11 @@
-import express from "express";
-import bodyParser from "body-parser";
+import express, { Request } from "express";
 import cors from "cors";
 import { v1Router } from "./v1Router";
 
 const app = express();
 const PORT = process.env.PORT || 1235;
 
-app.use(cors());
-app.use(bodyParser.json());
+app.use(cors({ origin: ["http://localhost:3000"] }));
 app.use(express.json());
 
 app.use("/api", v1Router);
